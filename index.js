f904021445b1c5327540f432e46ba7a316f893fc
Sharon <sharonirungu2006@gmail.com>
2025-06-22 18:13:53 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+
+function submitData(name, email) {
+  const formData = {
+    name: name,
+    email: email
+  };
+
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  })
+  .then(response => response.json())
+  .then(data => {
+    document.body.innerHTML = data.id;
+  })
+  .catch(error => {
+    document.body.innerHTML = error.message;
+  });
+}
